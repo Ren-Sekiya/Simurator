6b4743663f8583af55f57a6979eb57376719b9c3
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 00:32:19 +0900

    aaa

--- graph.js
+++ graph.js
@@ -48,12 +48,12 @@ class vgraph{
 }
 
     
-    document.querySelector("#b01").addEventListener('click', function(event) {
+    document.querySelector("#b01").addEventListener('click', () =>{
         var vg = new vgraph();
      vg.graph();
     });
 
-    document.querySelector("#b02").addEventListener('click', function(event) {
+    document.querySelector("#b02").addEventListener('click', () => {
         var vg = new vgraph();
         vg.graph2(x);
     });
