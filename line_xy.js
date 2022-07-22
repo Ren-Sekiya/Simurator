86070f71b010abc5f5ba5d5b9144ced46d1d90e8
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-22 21:09:23 +0900

    座標軸の設定

--- line_xy.js
+++ line_xy.js
@@ -2,6 +2,11 @@ var element = document.querySelector("#graph1");
 var context = element.getContext('2d');
 
 context.beginPath();
-context.moveTo(0, 0);
-context.lineTo(200, 200);
+context.moveTo(200, 0);
+context.lineTo(200, 400);
+context.stroke();
+
+context.beginPath();
+context.moveTo(0, 200);
+context.lineTo(400, 200);
 context.stroke();
