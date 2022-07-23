25a258a4a0e94796f792c43b8075c9da22ddffc6
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 14:14:18 +0900

    グラフの描画

--- graph.js
+++ graph.js
@@ -45,7 +45,7 @@ class vgraph{
          context2.moveTo(x1, y1);
 
          x2 = x1 + (width/width) * 20;
-         y2 = -2*((Math.pow(x, 3)/3) + 3*(Math.pow(x, 2)) - 8*x -10) + height/2;
+         y2 = -2*((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -10) + height/2;
 
          context.lineTo(x2, y2);
          context.stroke();
