f50056d738c0195f51ac9c851b8d54ee26e881bf
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 13:47:24 +0900

    aa

--- graph.js
+++ graph.js
@@ -9,22 +9,22 @@ class vgraph{
     graph() {
         var width = element.width;
         var height = element.height;
-        var x1 = width/2;
-        var y1 = 2*(3/*((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3) */)+ width/2;
+        var x1 = 0;
+        var y1 = height;
         var x2 = 0;
         var y2 = 0;
-       for(var x = 1 ;x < width/2; x++){
+       for(var x = -10 ;x < 10; x++){
          context.beginPath();
          context.moveTo(x1, y1);
          context2.beginPath();
          context2.moveTo(x1, y1);
 
-         x2 = x1 + (width/width) * 20;
-         y2 = -2*((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3) + height/2 ;
+         x2 = (width/width) * 20;
+         y2 = -2*((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3) + height ;
 
          context.lineTo(x2, y2);
          context.stroke();
-         context2.lineTo(x2 + 1, y2);
+         context2.lineTo(x2, y2);
          context2.stroke();
          x1 = x2;
          y1 = y2;
