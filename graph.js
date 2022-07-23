6af2f85abe2a5e7a9582817e4e929a7b857ced4e
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 14:00:37 +0900

    aaa

--- graph.js
+++ graph.js
@@ -20,7 +20,12 @@ class vgraph{
          context2.moveTo(x1, y1);
 
          x2 = x1 + (width/width) * 20;
-         y2 = -2*((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3) + height/2 ;
+         y2 = ((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3);
+         if(y2 < 0){
+            y2 = -2 * y2;
+         }else{
+            y2 = y2 * 2;
+         }
 
          context.lineTo(x2, y2);
          context.stroke();
