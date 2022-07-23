ba493433909cb2d23a4f60bb0fc056ba820cb5d9
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 16:05:07 +0900

    aa

--- graph.js
+++ graph.js
@@ -59,7 +59,7 @@ class vgraph{
 
     clear(){
         this.context.clearRect(0, 0, this.element.width, this.element.height);
-        this.context2.clearRect(0, 0, this.element2.width, ths.element2.height);
+        this.context2.clearRect(0, 0, this.element2.width, this.element2.height);
         this.context.beginPath();
     }
 }
