cdcd073b5942efef9a853919758fe65810d83fec
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 14:29:29 +0900

    aaa

--- line_xy.js
+++ line_xy.js
@@ -29,13 +29,12 @@ context2.beginPath();
 context2.moveTo(0, y1/2);
 context2.lineTo(x1, y1/2);
 context2.stroke();
+var vg = new vgraph();
 
 document.querySelector("#b01").addEventListener('click', () =>{
-    var vg = new vgraph();
- vg.graph();
+    vg.graph();
 });
 
 document.querySelector("#b02").addEventListener('click', () => {
-    var vg = new vgraph();
     vg.graph2();
 });
