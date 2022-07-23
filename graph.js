31f5afea5851e2015f6ef9cfebf2905ef9e959f8
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-23 12:50:13 +0900

    aaa

--- graph.js
+++ graph.js
@@ -7,40 +7,40 @@ class vgraph{
     context2 = element2.getContext('2d');
 
     graph() {
-        var x1 = 200.0;
-        var x2 = 200.0;
-        var y1 = 200.0;
-        var y2 = 200.0;
-        for(var x = 0.0 ;x < 10.0; x + 0.1){
+        var x1 = element.width;
+        var x2 = element2.width;
+        var y1 = element.height;
+        var y2 = element2.height;
+        for(var x = 0.0 ;x < 10.0; x++){
          var y2 = -((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3) + 200;
          context.beginPath();
          context.moveTo(x1, y1);
-         context.lineTo(x2 + 0.1, y2);
+         context.lineTo(x2 + 1, y2);
          context.stroke();
-         context2.beginPath();
+         /*context2.beginPath();
          context2.moveTo(x1, y1);
-         context2.lineTo(x2 + 0.1, y2);
-         context2.stroke();
+         context2.lineTo(x2 + 1, y2);
+         context2.stroke();*/
          x1 = x2;
          y1 = y2;
         }
     }
 
     graph2() {
-        var x1 = 200.0;
-        var x2 = 200.0;
-        var y1 = 200.0;
-        var y2 = 200.0;
-        for(var x = 0.0 ;x < 10.0; x + 0.1){
-         var y2 = -((Math.pow(x, 3)/3) + 3*(Math.pow(x, 2)) - 8*x -10) + 200;
+        var x1 = element.width;
+        var x2 = element2.width;
+        var y1 = element.height;
+        var y2 = element2.height;
+        for(var x = 0.0 ;x < 10.0; x++){
+         var y2 = -((Math.pow(x, 3)/3) - 3*(Math.pow(x, 2)) + 8*x -3) + 200;
          context.beginPath();
          context.moveTo(x1, y1);
-         context.lineTo(x2 + 0.1, y2);
+         context.lineTo(x2 + 1, y2);
          context.stroke();
-         context2.beginPath();
+         /*context2.beginPath();
          context2.moveTo(x1, y1);
-         context2.lineTo(x2 + 0.1, y2);
-         context2.stroke();
+         context2.lineTo(x2 + 1, y2);
+         context2.stroke();*/
          x1 = x2;
          y1 = y2;
         }
