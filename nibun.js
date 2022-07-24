30ea51abd1eae383677eb454ec96712c8cfdc4e7
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-24 17:40:37 +0900

    二分法

--- nibun.js
+++ nibun.js
@@ -1,18 +1,41 @@
 class calnibun{
-    constructor(elem1, elem2, elem3, elem4, elem5, elem6){
+    constructor(elem1, elem2, elem3, elem4, elem5, elem6, elem7){
         this.elem1 = elem1; //1つ目のグラフ
         this.elem2 = elem2; //二枚目のキャンバス
-        this.elem3 = elem3; //初期値a
-        this.elem4 = elem4; //初期値b
+        this.a = elem3.value; //初期値a
+        this.b = elem4.value; //初期値b
         this.elem5 = elem5; //理論値
         this.elem6 = elem6; //計算値
+        this.graph = elem7.number;
         this.answer = 0.44585
-        this.eps = 0.001;
+        this.eps = 0.0001;
         this.roop = 20;
     };
 
     calcurator(){
         this.elem5.innerHTML = "理論値：" + 0.44585;
+        if(this.graph == 1){
+            do{
+                var c = (a + b) / 2.0;
+                if(this.func(c)*this.funk(a) < 0) b = c;
+                else a = c;
+            }while(a - b > eps);
+        }
+        else{
+            do{
+                var c = (a + b) / 2.0;
+                if(this.func2(c)*this.funk2(a) < 0) b = c;
+                else a = c;
+            }while(a - b > eps);
+        }
+        this.elem6.innerHTML = func(c);
+    }
 
+    func(tmp){
+        return (Math.pow(tmp, 3)/3) - 3*(Math.pow(tmp, 2)) + 8*x -3;
+    }
+
+    funk2(tmp){
+        return (Math.pow(tmp, 3)/3) - 3*(Math.pow(tmp, 2)) + 8*x -10;
     }
 }
