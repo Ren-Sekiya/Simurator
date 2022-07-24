8769f7f287c861ed8690e8e9964cf824b01d3123
Ren-Sekiya <s2032083UK@s.chibakoudai.jp>
2022-07-24 23:05:33 +0900

    aa

--- nyuton.js
+++ nyuton.js
@@ -24,7 +24,7 @@ class calnyuton{
                 context.fillstyle = "red";
                 context.fill();
                 this.elem4.innerHTML = "ニュートン法の計算値：" + Math.round(this.a*100000)/100000;
-                if(Math.abs(this.a - this.b) < this.eps || timer > 300) break;
+                if(Math.abs(this.a - this.b) < this.eps || timer > 3) break;
                 else{
                     this.b = this.a;
                     timer++;
